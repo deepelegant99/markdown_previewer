d564468c8e46b22333350cb054f6876a61d6c9db
Henry Crossley <crossh2001@yahoo.com>
2022-03-02 22:47:44 -0800

    editor works with console log

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,9 +3,13 @@ import styles from "./Editor.module.css";
 
 
 export const Editor = () => {
+
+  function testHandle(evt){
+    console.log(evt.target.value);
+  }
   return (
     <div style={{backgroundColor:'blue', width:'30%', height:'40%'}}>Editor
-    <textarea id="Editor" className={styles.Editor}/>
+    <textarea id="Editor" className={styles.Editor}  onChange={testHandle}/>
     </div>
   )
 }
