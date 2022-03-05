2f25080bbdeaac81bee9cd20d13a58ef6fe2c030
Phillip <75212736+deepelegant99@users.noreply.github.com>
2022-03-04 23:38:17 -0800

    Comment out the console.log

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import styles from "./App.module.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Editor } from "./components/Editor";
 import { Preview } from "./components/Preview";
 
 function App() {
   const [text, setText] = useState("");
 
-  function editorHandler(event){
-    console.log(event.target.value);
+  function editorHandler(event) {
+    // console.log(event.target.value);
     setText(event.target.value);
   }
 
   return (
     <div className={styles.App}>
-      <Editor text={text} editorHandler ={editorHandler}/>
-      <Preview text={text}/>
+      <Editor text={text} editorHandler={editorHandler} />
+      <Preview text={text} />
     </div>
   );
 }
