77c10d7861a9b5da00985f4f6eddb73f8f2e997e
Phillip Wong <deepelegant99@gmail.com>
2022-03-09 23:27:55 -0800

    added incline code markdown and code block markdown on default editor content

--- src/App.js
+++ src/App.js
@@ -6,42 +6,25 @@ import { Preview } from "./components/Preview";
 function App() {
   const defaulttext = `
 
-  # Welcome to my React Markdown Previewer!
+# Welcome to my React Markdown Previewer!
+## This is a sub-heading...`
++
+` 
+This is how you do a link:
+[links](https://www.freecodecamp.org)
+` +
+`
+The followng is a inline code: 
+ \` d=2 \`
+` + 
+`
+The following is a code block: 
+ \`\`\`javascript I'm A tab
+console.log('Code Tab A');
+\`\`\`
+`; 
 
-## This is a sub-heading...
-### And here's some other cool stuff:
-
-
-
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
-
-There's also [links](https://www.freecodecamp.org), and
-> Block Quotes!
-
-And if you want to get really crazy, even tables:
-
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | -------------
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
-
-- And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
-
-
-1. And there are numbered lists too.
-1. Use just 1s if you want!
-1. And last but not least, let's not forget embedded images:
-
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
-
-  
-  `;
+ 
   
   const [text, setText] = useState(defaulttext); 
 
